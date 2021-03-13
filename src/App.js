498a4f079087253d5cbc8572c49cbcda80bf21dd
Terrance Lee <takhol@andrew.cmu.edu>
2021-03-13 18:19:08 +0900

    learning about mapping

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+const favFoods = [
+  {id: 1,
+    name: "kimchi",
+    spicy: true},
+  {id: 2,
+    name: "dontaktsu",
+    spicy: false},
+  {id:3,
+    name: "삼겹",
+    spicy: false},
+  {id: 4,
+    name: "ramen",
+    spicy: true},
+  {id: 5,
+    name: "life",
+    spicy: true}];
+    
+function Food(props){
+  return <div>
+    <h1>I like {props.name}</h1>
+    <h2>It is {props.spicy? "spicy": "not spicy"}</h2>
+    </div>
+}
+
+function renderFood(dish){
+  return <Food key = {dish.id} name = {dish.name} spicy = {dish.spicy}/>
+}
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-      
+      {favFoods.map(renderFood)}
     </div>
   );
 }
