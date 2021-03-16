a7f88e7beb867bf8dd10c150f8f31974c0133425
Terrance Lee <takhol@andrew.cmu.edu>
2021-03-16 15:36:00 +0900

    deleted food, learned states

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const favFoods = [
-  {id: 1,
-    name: "kimchi",
-    spicy: true},
-  {id: 2,
-    name: "dontaktsu",
-    spicy: false},
-  {id:3,
-    name: "삼겹",
-    spicy: false},
-  {id: 4,
-    name: "ramen",
-    spicy: true},
-  {id: 5,
-    name: "life",
-    spicy: true}];
-    
-function Food(props){
-  return <div>
-    <h1>I like {props.name}</h1>
-    <h2>It is {props.spicy? "spicy": "not spicy"}</h2>
-    </div>
-}
+class App extends React.Component{
+  //data that will change
+  state = {
+    count: 0
+  };
 
-function renderFood(dish){
-  return <Food key = {dish.id} name = {dish.name} spicy = {dish.spicy}/>
-}
+  add = () => {
+    this.setState(current => ({count: current.count+1}))
+  };
+  subtract = () => {
+    this.setState(current => ({count: current.count-1}))
+  };
+  //dont get current state using this.state.count => not good practice
+  //this.setState(current => ({count: current.count+1}))
 
 
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      {favFoods.map(renderFood)}
-    </div>
-  );
+  render(){
+    return <div>
+      <h1>The count is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.subtract}>Substract</button>
+      </div>
+  } //parathanses immediate, without it, only onClick\
 }
 
 export default App;
